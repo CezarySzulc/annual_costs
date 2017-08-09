552e4c413f89c85f0ca84caa3e96b686dce23d87
Cezary Szulc <cezary.szulc@gmail.com>
2017-08-09 20:48:26 +0200

    add Ridge model

--- predict_values.py
+++ predict_values.py
@@ -3,8 +3,14 @@
 @author: C
 """
 
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import (
+                                train_test_split, 
+                                GridSearchCV)
+from sklearn.linear_model import (
+                                LinearRegression, 
+                                Lasso, 
+                                Ridge)
+                                
 from display_annual_costs import Raport
 import numpy as np
 
@@ -28,15 +34,29 @@ class Predict(Raport):
                                     test_size=0.3,
                                     random_state=42)
                 
-    def create_model(self):
-        """ creeate model and test it """
+    def create_linear_regression_model(self):
+        """ crate Linear Regression model and test it """
 
         model = LinearRegression()
         model.fit(self.X_train, self.y_train)
         score = model.score(self.X_test, self.y_test)
-        print("R^2: {}".format(score))
+        print('Linear regression model:') 
+        print('score:\t\t{}'.format(score))
+        
+    def create_ridge_model(self):
+        """ create ridge model and test it """
+        
+        param_grid = {'alpha': np.arange(0, 2, 0.1)}
+        
+        model = GridSearchCV(Ridge(), param_grid)
+        model.fit(self.X_train, self.y_train)
+        score = model.score(self.X_test, self.y_test)
+        print('Ridge score:')
+        print('best param \t{}'.format(model.best_params_))
+        print('score:\t\t{}'.format(score))
 
 if __name__ == '__main__':       
     cost_predict = Predict()
     cost_predict.prepare_data()
-    cost_predict.create_model()
+    cost_predict.create_linear_regression_model()
+    cost_predict.create_ridge_model()
